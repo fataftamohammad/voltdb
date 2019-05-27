de9eeb4923abeb7d5bd459cad87c74d4131d8d89
fataftamohammad <fataftamohammad@gmail.com>
2019-05-27 11:47:32 -0400

    gen

--- bin/gen.cpp
+++ bin/gen.cpp
@@ -3,11 +3,27 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	int count = stoi(argv[1]);
 
-	for (int i = 0; i < count; ++i)
+	for (int i = 1; i < argc; ++i)
 	{
-		printf("insert into temp values(%d,'%d');\n",i,i );
+		//insert
+		if(argv[i][0]=='-' && argv[i][1] == 'i')
+		{
+			int count = stoi(argv[++i]);
+			for (int i = 0; i < count; ++i)
+				printf("insert into temp values(%d,'%d');\n",i,i );
+		}
+		//update
+		if(argv[i][0]=='-' && argv[i][1] == 'u')
+		{
+			int count = stoi(argv[++i]);
+			for (int i = 0; i < count; ++i)
+				printf("update temp set name='x';\n");
+		}
+
 	}
+	
+
+
 	return 0;
 }
